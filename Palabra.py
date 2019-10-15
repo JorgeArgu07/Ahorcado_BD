e12cfde2e2b7dc00ea7716b72b73cd01aa5be8cf
Eduardo Favela <arturo99favela@gmail.com>
2019-10-15 13:07:06 -0500

    Actualización final (creo)

--- Palabra.py
+++ Palabra.py
@@ -1,9 +1,63 @@
 from random import shuffle
 from Dibujo import Dibujo
+from BD import BD
 import mysql.connector
 from mysql.connector import Error
 import os
 class Palabra:
+	def llenarbasedatos(self):
+		palabras = [line.rstrip() for line in open("Palabras.txt")]
+		if len(palabras) == 0:
+			self.obtenerPalabras()
+			palabras = [line.rstrip() for line in open("Palabras.txt")]
+		shuffle(palabras)
+		conexion = BD()
+		success = conexion.llenarbd(palabras)
+		return success
+
+	def agregarpalabradb(self):
+		conexion = BD()
+		archivopalabras = open("Palabras_todas.txt", "r+")
+		otra = "s"
+		while otra == "s":
+			palabra = input("Escriba la palabra que desea añadir y presione Enter para agregarla\n")
+			success = conexion.agregarpalabra(palabra)
+			archivopalabras.readlines()
+			archivopalabras.write("\n" + palabra.lower())
+			if success == 1:
+				print("Palabra añadida")
+			else:
+				print("Ocurrió un error: {}".format(success))
+			otra = input("¿Desea añadir otra palabra?(s/n)")
+		archivopalabras.close()
+
+	def reiniciarpalabrasbd(self):
+		conexion = BD()
+		palabras = [line.rstrip() for line in open("Palabras_todas.txt")]
+		if len(palabras) == 0:
+			print("No se encontraron palabras")
+			self.agregarpalabradb()
+		conexion.llenarbd(palabras)
+		palabras2 = open("Palabras.txt", "w")
+		nuevaLista = "\n".join(str(x) for x in palabras)
+		palabras2.write(nuevaLista)
+		palabras2.close()
+
+	def getpalabrabd(self):
+		palabras = []
+		conexion = BD()
+		for palabra in conexion.getpalabras():
+			palabras.append(palabra)
+		if len(palabras) == 0:
+			self.reiniciarpalabrasbd()
+		shuffle(palabras)
+		palabra = palabras.pop()
+		conexion.llenarbd(palabras)
+		palabras2 = open("Palabras.txt", "w")
+		nuevaLista = "\n".join(str(x) for x in palabras)
+		palabras2.write(nuevaLista)
+		palabras2.close()
+		self.separarPalabra(palabra)
 
 	def getPalabra(self):
 		palabras=[line.rstrip() for line in open("Palabras.txt")]
@@ -28,7 +82,7 @@ class Palabra:
 			archivoPalabras.readlines()
 			archivoPalabras.write("\n"+palabra.lower())
 			print("Palabra añadida")
-			otra = input ("¿Desea añadir otra palabra?(s/n)")
+			otra = input("¿Desea añadir otra palabra?(s/n)")
 
 		archivoPalabras.close()
 
