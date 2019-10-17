e29570641bf6055a632a68ef629d4fa6a840205c
Eduardo Favela <arturo99favela@gmail.com>
2019-10-17 14:54:40 -0500

    update BD

--- BD.py
+++ BD.py
@@ -21,9 +21,11 @@ class BD:
         except Error as error:
             er = error
             success = 0
-
         finally:
-            return success
+            if success != 1:
+                return er
+            else:
+                return success
 
     def agregarpalabra(self, palabra):
         success = 0
@@ -82,37 +84,49 @@ class BD:
             else:
                 return success
 
-    def cambiarvalidacion(self, palabra):
-    	success = 0
-    	er = ""
-    	try:
-    		conexion = mysql.connector.connect(host ="localhost", user="root", passwd="", db="ahorcado_BD")
-    		query = "select id from ahorcado_bd.palabras where palabra ='"+palabra+"'"
-    		cursor = conexion.cursor(buffered=True)
-    		cursor.execute(query)
-    		conexion.commit()
-    		consulta = cursor.fetchone()
-    		identificador=0
-    		for row in consulta:
-    			identificador=row[0]
+    def cambiarvalidacion2(self, identificador):
+        success = 0
+        er = ""
+        try:
+            conexion = mysql.connector.connect(host="localhost", user="root", passwd="", db="ahorcado_BD")
+            query = "update ahorcado_bd.palabras set validacion=1 where id='"+identificador+"'"
+            cursor = conexion.cursor()
+            cursor.execute(query)
+            success = 1
+        except Error as error:
+            print("hola", success, er)
+            success = 0
+        finally:
+            if success != 1:
+                return er
+            else:
+                return success
 
-    		print(identificador)
-    		query2 = "update palabras set validacion = 1 where id ='"+identificador+"'"
-    		cursor.execute(query2)
-    		conexion.commit()
-    		cursor.close()
-    		success =1
-    	except Error as error:
-    		er = error
-    		success = 0
-    	finally:
-    		print("hola",success, er)
-    		if success!=1:
-    			return er
-    		else:
-    			return success
-    	 
+    def cambiarvalidacion(self, palabra):
+        success = 0
+        er = ""
+        try:
+            conexion = mysql.connector.connect(host ="localhost", user="root", passwd="", db="ahorcado_BD")
+            query = "select id from ahorcado_bd.palabras where palabra ='"+palabra+"'"
+            cursor = conexion.cursor()
+            cursor.execute(query)
+            consulta = cursor.fetchall()
+            identificador = 0
+            for row in consulta:
+                identificador = row[0]
+            # print(identificador)
+            self.cambiarvalidacion2(identificador)
+            success = 1
+        except Error as error:
+            print("hola", success, er)
+            success = 0
+        finally:
+            if success != 1:
+                return er
+            else:
+                return success
+
 
 bd = BD()
-bd.cambiarvalidacion("mesa")
+bd.cambiarvalidacion2(7)
 
