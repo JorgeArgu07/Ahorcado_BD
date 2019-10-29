3f9543c326af664b92f93c224cf1c67d8b5e9454
Eduardo Favela <arturo99favela@gmail.com>
2019-10-28 22:05:01 -0600

    ahorcado terminado

--- BD.py
+++ BD.py
@@ -48,6 +48,28 @@ class BD:
             else:
                 return success
 
+    def reiniciarusadas(self):
+        success = 0
+        er = ""
+        try:
+            conexion=mysql.connector.connect(host="localhost",user="root",passwd="", db="ahorcado_BD")
+            cursor = conexion.cursor()
+            cursor.execute("select id from ahorcado_bd.palabras")
+            consulta = cursor.fetchall()
+            for i in consulta:
+                cursor.execute("UPDATE palabras SET validacion = 0 WHERE id ={}".format(i[0]))
+            conexion.commit()
+
+        except Error as error:
+            success = 0
+            er = error
+            print("hola", er)
+        finally:
+            if success != 1:
+                return er
+            else:
+                return success
+
     def getpalabras(self):
         palabras = []
         success = 0
@@ -57,9 +79,12 @@ class BD:
             cursor = conexion.cursor()
             cursor.execute("select * from palabras where validacion = 0")
             consulta = cursor.fetchall()
-            for row in consulta:
-                palabras.append(row[1])
-            success = 1
+            if len(consulta) == 0:
+                success = 0
+            else:
+                for row in consulta:
+                    palabras.append(row[1])
+                success = 1
         except Error as e:
             success = 0
             er = e
@@ -69,33 +94,18 @@ class BD:
             else:
                 print("Ocurrió un error: {}".format(er))
 
-    def conectar(self):
+    def validacionaux(self, identificador):
         success = 0
         er = ""
         try:
             conexion = mysql.connector.connect(host="localhost", user="root", passwd="", db="ahorcado_BD")
+            cursor = conexion.cursor()
+            query = "UPDATE palabras SET validacion = 1 WHERE id ={}".format(identificador)
+            cursor.execute(query)
+            conexion.commit()
             success = 1
         except Error as error:
             er = error
-            success = 0
-        finally:
-            if success != 1:
-                return er
-            else:
-                return success
-
-    def cambiarvalidacion2(self, identificador):
-        success = 0
-        er = ""
-        try:
-            conexion = mysql.connector.connect(host="localhost", user="root", passwd="", db="ahorcado_BD")
-            query = "update ahorcado_bd.palabras set validacion=1 where id="+identificador
-            cursor = conexion.cursor()
-            cursor.execute(query)
-            cursor.commit()
-            success = 1
-        except Error as error:
-            print("hola", success, error)
             success = 0
         finally:
             if success != 1:
@@ -115,11 +125,10 @@ class BD:
             identificador = 0
             for row in consulta:
                 identificador = row[0]
-            # print(identificador)
-            self.cambiarvalidacion2(identificador)
+            self.validacionaux(identificador)
             success = 1
         except Error as error:
-            print("hola", success, er)
+            er = error
             success = 0
         finally:
             if success != 1:
--- Palabra.py
+++ Palabra.py
@@ -5,54 +5,54 @@ import mysql.connector
 from mysql.connector import Error
 import os
 class Palabra:
-	def llenarbasedatos(self):
-		palabras = [line.rstrip() for line in open("Palabras.txt")]
-		if len(palabras) == 0:
-			self.obtenerPalabras()
-			palabras = [line.rstrip() for line in open("Palabras.txt")]
-		shuffle(palabras)
-		conexion = BD()
-		success = conexion.llenarbd(palabras)
-		return success
+    def llenarbasedatos(self):
+        palabras = [line.rstrip() for line in open("Palabras.txt")]
+        if len(palabras) == 0:
+            self.obtenerPalabras()
+            palabras = [line.rstrip() for line in open("Palabras.txt")]
+        shuffle(palabras)
+        conexion = BD()
+        success = conexion.llenarbd(palabras)
+        return success
 
-	def agregarpalabradb(self):
-		conexion = BD()
-		archivopalabras = open("Palabras_todas.txt", "r+")
-		otra = "s"
-		while otra == "s":
-			palabra = input("Escriba la palabra que desea añadir y presione Enter para agregarla\n")
-			success = conexion.agregarpalabra(palabra)
-			archivopalabras.readlines()
-			archivopalabras.write("\n" + palabra.lower())
-			if success == 1:
-				print("Palabra añadida")
-			else:
-				print("Ocurrió un error: {}".format(success))
-			otra = input("¿Desea añadir otra palabra?(s/n)")
-		archivopalabras.close()
+    def agregarpalabradb(self):
+        conexion = BD()
+        archivopalabras = open("Palabras_todas.txt", "r+")
+        otra = "s"
+        while otra == "s":
+            palabra = input("Escriba la palabra que desea añadir y presione Enter para agregarla\n")
+            success = conexion.agregarpalabra(palabra)
+            archivopalabras.readlines()
+            archivopalabras.write("\n" + palabra.lower())
+            if success == 1:
+                print("Palabra añadida")
+            else:
+                print("Ocurrió un error: {}".format(success))
+            otra = input("¿Desea añadir otra palabra?(s/n)")
+        archivopalabras.close()
 
-	def reiniciarpalabrasbd(self):
+    def reiniciarpalabrasbd(self):
             conexion = BD()
             palabras = [line.rstrip() for line in open("Palabras_todas.txt")]
             if len(palabras) == 0:
                 print("No se encontraron palabras")
                 self.agregarpalabradb()
                 palabras = [line.rstrip() for line in open("Palabras_todas.txt")]
-            conexion.llenarbd(palabras)
+            conexion.reiniciarusadas()
             palabras2 = open("Palabras.txt", "w")
             nuevaLista = "\n".join(str(x) for x in palabras)
             palabras2.write(nuevaLista)
             palabras2.close()
 
-	def getpalabrabd(self):
-	    	palabras = []
-	    	conexion = BD()
-	    	for palabra in conexion.getpalabras():
-        	    palabras.append(palabra)
+    def getpalabrabd(self):
+            palabras = []
+            conexion = BD()
+            for palabra in conexion.getpalabras():
+                palabras.append(palabra)
             if len(palabras) == 0:
-        		self.reiniciarpalabrasbd()
+                self.reiniciarpalabrasbd()
                 for palabra in conexion.getpalabras():
-					palabras.append(palabra)
+                    palabras.append(palabra)
             shuffle(palabras)
             palabra = palabras.pop()
             conexion.cambiarvalidacion(palabra)
@@ -62,122 +62,122 @@ class Palabra:
             palabras2.close()
             self.separarPalabra(palabra)
 
-	def getPalabra(self):
-		palabras=[line.rstrip() for line in open("Palabras.txt")]
-		if len(palabras) == 0:
-			self.obtenerPalabras()
-			palabras=[line.rstrip() for line in open("Palabras.txt")]
-		shuffle(palabras)
-		palabra = palabras.pop()
-		palabras2 = open("Palabras.txt", "w")
-		nuevaLista = "\n".join(str(x) for x in palabras)
-		palabras2.write(nuevaLista)
-		palabras2.close()
-		self.separarPalabra(palabra)
+    def getPalabra(self):
+        palabras=[line.rstrip() for line in open("Palabras.txt")]
+        if len(palabras) == 0:
+            self.obtenerPalabras()
+            palabras=[line.rstrip() for line in open("Palabras.txt")]
+        shuffle(palabras)
+        palabra = palabras.pop()
+        palabras2 = open("Palabras.txt", "w")
+        nuevaLista = "\n".join(str(x) for x in palabras)
+        palabras2.write(nuevaLista)
+        palabras2.close()
+        self.separarPalabra(palabra)
 
 
-	def añadirPalabra(self):
-		archivoPalabras = open("Palabras_todas.txt", "r+")
-		otra = "s"
-		while otra == "s":
-			os.system('cls')
-			palabra = input("Escriba la palabra que desea añadir y presione Enter para agregarla\n")
-			archivoPalabras.readlines()
-			archivoPalabras.write("\n"+palabra.lower())
-			print("Palabra añadida")
-			otra = input("¿Desea añadir otra palabra?(s/n)")
-		archivoPalabras.close()
+    def añadirPalabra(self):
+        archivoPalabras = open("Palabras_todas.txt", "r+")
+        otra = "s"
+        while otra == "s":
+            os.system('cls')
+            palabra = input("Escriba la palabra que desea añadir y presione Enter para agregarla\n")
+            archivoPalabras.readlines()
+            archivoPalabras.write("\n"+palabra.lower())
+            print("Palabra añadida")
+            otra = input("¿Desea añadir otra palabra?(s/n)")
+        archivoPalabras.close()
 
 
-	def obtenerPalabras(self):
-		palabras = [line.rstrip() for line in open("Palabras_todas.txt")]
-		if len(palabras) == 0:
-			print("No se encontraron palabras")
-			self.añadirPalabra()
-		palabras2 = open("Palabras.txt", "w")
-		nuevaLista = "\n".join(str(x) for x in palabras)
-		palabras2.write(nuevaLista)
-		palabras2.close()
+    def obtenerPalabras(self):
+        palabras = [line.rstrip() for line in open("Palabras_todas.txt")]
+        if len(palabras) == 0:
+            print("No se encontraron palabras")
+            self.añadirPalabra()
+        palabras2 = open("Palabras.txt", "w")
+        nuevaLista = "\n".join(str(x) for x in palabras)
+        palabras2.write(nuevaLista)
+        palabras2.close()
 
-	def separarPalabra(self, palabra):
-		palabraDestapada = list(palabra)
-		palabraTapada = []
+    def separarPalabra(self, palabra):
+        palabraDestapada = list(palabra)
+        palabraTapada = []
 
-		for letra in palabraDestapada:
-			palabraTapada.append(" _")
-
-		
-		self.compararPalabra(palabraTapada, palabraDestapada)
-
-		
-
-	def compararPalabra(self, palabraTapada, palabraDestapada):
-
-		os.system('cls')
-		palabra = self.concatLista(palabraDestapada)
-		intentos = 5
-		ganador = True
-
-		dibujo = Dibujo()
-		actual = dibujo.dibujarHorca()
-
-		while self.concatLista(palabraTapada) != self.concatLista(palabraDestapada):
-
-			actual
-
-			print(self.concatLista(palabraDestapada))			
-			print(self.concatLista(palabraTapada))
-			print("Tienes "+str(intentos)+" intentos")
-			letraIngresada = input("Escribe una letra o una palabra\n")
-			letraIngresada.lower()
-
-			if letraIngresada == palabra:
-				palabraTapada = palabra
-			else:
-				posiciones = [i for i,l in enumerate(palabraDestapada) if l == letraIngresada]
-				if len(posiciones) > 0 :
-					#enumerate(palabraTapada)
-					for p in posiciones:
-						palabraTapada[p] = letraIngresada
-					os.system('cls')
-					print("Acertaste!")
+        for letra in palabraDestapada:
+            palabraTapada.append(" _")
 
 
-				else:
-					intentos -= 1
-					if intentos == 4:
-						os.system('cls')
-						actual = dibujo.dibujarCabeza()
-						#print(palabraTapada)
-					elif intentos == 3:	
-						os.system('cls')
-						actual = dibujo.dibujarBrazo1()
-						#print(palabraTapada)
-					elif intentos == 2:
-						os.system('cls')
-						actual = dibujo.dibujarBrazo2()
-						#print(palabraTapada)
-					elif intentos == 1:
-						os.system('cls')
-						actual = dibujo.dibujarPierna1()
-						#print(palabraTapada)
-					elif intentos == 0:
-						os.system('cls')
-						palabraTapada = palabraDestapada
-						ganador = False
+        self.compararPalabra(palabraTapada, palabraDestapada)
 
 
-		if ganador:
-			print("Adivinaste la palabra. Ganaste!")
-		else:
-			dibujo.dibujarPierna2()
+
+    def compararPalabra(self, palabraTapada, palabraDestapada):
+
+        os.system('cls')
+        palabra = self.concatLista(palabraDestapada)
+        intentos = 5
+        ganador = True
+
+        dibujo = Dibujo()
+        actual = dibujo.dibujarHorca()
+
+        while self.concatLista(palabraTapada) != self.concatLista(palabraDestapada):
+
+            actual
+
+            print(self.concatLista(palabraDestapada))
+            print(self.concatLista(palabraTapada))
+            print("Tienes "+str(intentos)+" intentos")
+            letraIngresada = input("Escribe una letra o una palabra\n")
+            letraIngresada.lower()
+
+            if letraIngresada == palabra:
+                palabraTapada = palabra
+            else:
+                posiciones = [i for i,l in enumerate(palabraDestapada) if l == letraIngresada]
+                if len(posiciones) > 0 :
+                    #enumerate(palabraTapada)
+                    for p in posiciones:
+                        palabraTapada[p] = letraIngresada
+                    os.system('cls')
+                    print("Acertaste!")
 
 
-	def concatLista(self, lista):
-		palabra = ""
-		for l in lista:
-			palabra+= l
-		return palabra
+                else:
+                    intentos -= 1
+                    if intentos == 4:
+                        os.system('cls')
+                        actual = dibujo.dibujarCabeza()
+                        #print(palabraTapada)
+                    elif intentos == 3:
+                        os.system('cls')
+                        actual = dibujo.dibujarBrazo1()
+                        #print(palabraTapada)
+                    elif intentos == 2:
+                        os.system('cls')
+                        actual = dibujo.dibujarBrazo2()
+                        #print(palabraTapada)
+                    elif intentos == 1:
+                        os.system('cls')
+                        actual = dibujo.dibujarPierna1()
+                        #print(palabraTapada)
+                    elif intentos == 0:
+                        os.system('cls')
+                        palabraTapada = palabraDestapada
+                        ganador = False
+
+
+        if ganador:
+            print("Adivinaste la palabra. Ganaste!")
+        else:
+            dibujo.dibujarPierna2()
+
+
+    def concatLista(self, lista):
+        palabra = ""
+        for l in lista:
+            palabra+= l
+        return palabra
 
 
 pa=Palabra()
